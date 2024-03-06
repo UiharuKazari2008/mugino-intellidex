5d3c23a0a48f3df7c93df0399943a4ebe3bba000
Yukimi Kazari <kazari@acr.moe>
2024-03-06 04:45:36 -0500

    Added NSFW classifications

--- localhost.js
+++ localhost.js
@@ -923,11 +923,18 @@
         }
 
         const sqlOrderBy = (analyzerGroup && analyzerGroup.order) ? analyzerGroup.order :'eid DESC'
-        const query = `SELECT x.*, y.host, y.path_hint, y.preview_hint, y.full_hint FROM (SELECT ${sqlFields.join(', ')} FROM ${sqlTables.join(', ')} WHERE (${sqlWhereBase.join(' AND ')} AND (${sqlWhereFiletypes.join(' OR ')}))${(sqlWhereFilter.length > 0) ? ' AND (' + sqlWhereFilter.join(' AND ') + ')' : ''} ORDER BY ${sqlOrderBy} LIMIT ${(analyzerGroup && analyzerGroup.limit) ? analyzerGroup.limit : 100}) x LEFT JOIN (SELECT eid, host, path_hint, preview_hint, full_hint FROM kanmi_records_cdn WHERE host = ${systemglobal.cdn_id} AND (preview_hint IS NOT NULL OR full_hint IS NOT NULL)) y ON (x.eid = y.eid)`
+        const query = `SELECT x.*, y.host, y.path_hint, y.preview_hint, y.full_hint, y.mfull_hint FROM (SELECT ${sqlFields.join(', ')} FROM ${sqlTables.join(', ')} WHERE (${sqlWhereBase.join(' AND ')} AND (${sqlWhereFiletypes.join(' OR ')}))${(sqlWhereFilter.length > 0) ? ' AND (' + sqlWhereFilter.join(' AND ') + ')' : ''} ORDER BY ${sqlOrderBy} LIMIT ${(analyzerGroup && analyzerGroup.limit) ? analyzerGroup.limit : 100}) x LEFT JOIN (SELECT eid, host, path_hint, preview_hint, full_hint, mfull_hint FROM kanmi_records_cdn WHERE host = ${systemglobal.cdn_id} AND (preview_hint IS NOT NULL OR full_hint IS NOT NULL OR mfull_hint IS NOT NULL)) y ON (x.eid = y.eid)`
         console.log(`Selecting data for analyzer group...`, analyzerGroup);
 
         const messages = (await sqlPromiseSafe(query, true)).rows.map(e => {
-            const url = `${systemglobal.cdn_access_url}preview/${e.path_hint}/${e.preview_hint || e.full_hint}`;
+            let url
+            if (e.preview_hint) {
+                url = `${systemglobal.cdn_access_url}preview/${e.path_hint}/${e.preview_hint}`;
+            } else if (e.full_hint) {
+                url = `${systemglobal.cdn_access_url}full/${e.path_hint}/${e.full_hint}`;
+            } else if (e.mfull_hint) {
+                url = `${systemglobal.cdn_access_url}master/${e.path_hint}/${e.mfull_hint}`;
+            }
             console.log(url)
             return { url, ...e };
         })
